906d4aa8256703b10f42b634fb709d07f9b6d1ac
xiaoxiaoyi <xiaoxiaoyi1026@gmail.com>
2023-01-26 14:55:44 +0800

    iLearn-content-Controller层&HttpClient测试

--- iLearn-content/iLearn-content-api/src/main/java/com/ilearn/content/api/CourseBaseInfoController.java
+++ iLearn-content/iLearn-content-api/src/main/java/com/ilearn/content/api/CourseBaseInfoController.java
@@ -4,14 +4,16 @@ import com.ilearn.base.model.PageParams;
 import com.ilearn.base.model.PageResult;
 import com.ilearn.content.model.dto.QueryCourseParamsDto;
 import com.ilearn.content.model.po.CourseBase;
+import com.ilearn.content.service.CourseBaseInfoService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 /**
  * @author xiaoxiaoyi
  * @version 1.0
- * @description TODO 课程基本信息相关接口
+ * @description 课程基本信息相关接口
  * @date 1/24/2023 5:03 PM
  */
 
@@ -19,11 +21,14 @@ import org.springframework.web.bind.annotation.*;
 @Api(value = "课程管理", tags = "课程管理相关接口")
 public class CourseBaseInfoController {
 
+    @Autowired
+    CourseBaseInfoService courseBaseInfoService;
+
     @PostMapping("/course/list")
     @ApiOperation("课程分页查询")
     public PageResult<CourseBase> list(PageParams pageParams, @RequestBody QueryCourseParamsDto queryCourseParamsDto) {
         // Controller -> Service -> Mapper(dao)
-        return null;
+        return courseBaseInfoService.queryCourseBaseList(pageParams, queryCourseParamsDto);
     }
 
 }
