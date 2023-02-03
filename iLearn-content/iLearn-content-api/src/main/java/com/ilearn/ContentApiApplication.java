f8f2a07592d7bfb5beda67576662ed58b289f61a
xiaoxiaoyi <xiaoxiaoyi1026@gmail.com>
2023-02-03 14:39:51 +0800

    配置文件优先级测试

--- iLearn-content/iLearn-content-api/src/main/java/com/ilearn/ContentApiApplication.java
+++ iLearn-content/iLearn-content-api/src/main/java/com/ilearn/ContentApiApplication.java
@@ -1,8 +1,10 @@
 package com.ilearn;
 
 import com.spring4all.swagger.EnableSwagger2Doc;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author xiaoxiaoyi
@@ -13,6 +15,30 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @EnableSwagger2Doc
 @SpringBootApplication
 public class ContentApiApplication {
+
+    @Value("${test-config.a}")
+    private String a;
+    @Value("${test-config.b}")
+    private String b;
+    @Value("${test-config.c}")
+    private String c;
+    @Value("${test-config.d}")
+    private String d;
+
+    /**
+     * 获取测试配置文件的内容
+     *
+     * @return 配置文件的测试编号
+     */
+    @Bean
+    public Integer getTest() {
+        // 测试结果: 远程配置 > 扩展远程配置 > 通用远程配置 > 本地配置
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
+        return 1;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ContentApiApplication.class, args);
