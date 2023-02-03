a767001bb1dfa0e3841770fd3d4a7070de961305
xiaoxiaoyi <xiaoxiaoyi1026@gmail.com>
2023-02-03 15:10:37 +0800

    设置网关nacos远程配置服务

--- iLearn-gateway/src/main/java/com/ilearn/GatewayApplication.java
+++ iLearn-gateway/src/main/java/com/ilearn/GatewayApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ILearnGatewayApplication {
+public class GatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ILearnGatewayApplication.class, args);
+        SpringApplication.run(GatewayApplication.class, args);
     }
 
 }
