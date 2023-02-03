3bc42b83c185394becf9e0083e91a3f492d0f2ce
xiaoxiaoyi <xiaoxiaoyi1026@gmail.com>
2023-02-03 14:49:45 +0800

    新建网关Gateway工程

---
+++ iLearn-gateway/src/main/java/com/ilearn/ILearnGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.ilearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ILearnGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ILearnGatewayApplication.class, args);
+    }
+
+}
