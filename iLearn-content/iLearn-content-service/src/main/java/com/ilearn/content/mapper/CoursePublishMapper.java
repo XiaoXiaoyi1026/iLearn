b1c4e41931259b63260615ed083cb538cc63c0b9
xiaoxiaoyi <xiaoxiaoyi1026@gmail.com>
2023-02-17 16:28:29 +0800

    使用generator生成发布和预发布两张表的mapper和po类

---
+++ iLearn-content/iLearn-content-service/src/main/java/com/ilearn/content/mapper/CoursePublishMapper.java
@@ -0,0 +1,15 @@
+package com.ilearn.content.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ilearn.content.model.po.CoursePublish;
+
+/**
+ * <p>
+ * 课程发布 Mapper 接口
+ * </p>
+ *
+ * @author xiaoxiaoyi
+ */
+public interface CoursePublishMapper extends BaseMapper<CoursePublish> {
+
+}
---
+++ iLearn-content/iLearn-content-service/src/main/java/com/ilearn/content/mapper/CoursePublishPreMapper.java
@@ -0,0 +1,15 @@
+package com.ilearn.content.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ilearn.content.model.po.CoursePublishPre;
+
+/**
+ * <p>
+ * 课程发布 Mapper 接口
+ * </p>
+ *
+ * @author xiaoxiaoyi
+ */
+public interface CoursePublishPreMapper extends BaseMapper<CoursePublishPre> {
+
+}
