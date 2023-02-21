4169779b0feab18564e3e9e4bf800237d114ad5f
xiaoxiaoyi <xiaoxiaoyi1026@gmail.com>
2023-02-21 15:42:46 +0800

    Feign远程服务调用异常处理方式2: 通过下游熔断, 上游fallbackFactory回调(可以拿到异常信息)进行降级处理

---
+++ iLearn-content/iLearn-content-service/src/main/java/com/ilearn/content/feign/MediaServiceClientFallbackFactory.java
@@ -0,0 +1,26 @@
+package com.ilearn.content.feign;
+
+import feign.hystrix.FallbackFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author xiaoxiaoyi
+ * @version 1.0
+ * @description Feign远程调用媒体服务的降级Factory
+ * @date 2/21/2023 3:35 PM
+ */
+@Slf4j
+@Component
+public class MediaServiceClientFallbackFactory implements FallbackFactory<MediaServiceClient> {
+    @Override
+    public MediaServiceClient create(Throwable throwable) {
+        // 通过FallbackFactory可以拿到下游熔断的错误信息
+        return (fileData, folder, objectName) -> {
+            // 降级方法, 可以记录错误日志
+            log.debug("Feign远程调用iLearn-media-api服务出现熔断异常, 异常信息: {}", throwable.getMessage());
+            // 快速返回, 避免上游服务被下游牵连
+            return null;
+        };
+    }
+}
