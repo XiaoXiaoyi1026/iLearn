a2b5a4f3f57403ec239ca2eb57607c3bdd26bd31
xiaoxiaoyi <xiaoxiaoyi1026@gmail.com>
2023-03-05 14:24:05 +0800

    微信授权码获取令牌

--- iLearn-authorize/src/main/java/com/ilearn/users/service/impl/WXAuthorize.java
+++ iLearn-authorize/src/main/java/com/ilearn/users/service/impl/WXAuthorize.java
@@ -3,6 +3,7 @@ package com.ilearn.users.service.impl;
 import com.ilearn.base.exception.ILearnException;
 import com.ilearn.base.mapper.UserAuthorities;
 import com.ilearn.base.mapper.UserRole;
+import com.ilearn.base.utils.JsonUtil;
 import com.ilearn.users.mapper.IlearnUserMapper;
 import com.ilearn.users.model.dto.AuthorizeInfo;
 import com.ilearn.users.model.dto.ILearnUserAuthorities;
@@ -12,7 +13,11 @@ import com.ilearn.users.service.AuthorizeService;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Map;
 
@@ -27,9 +32,22 @@ public class WXAuthorize implements AuthorizeService {
 
     private IlearnUserMapper ilearnUserMapper;
 
+    private RestTemplate restTemplate;
+
+    @Value("authorize.appID")
+    private String appID;
+
+    @Value("authorize.secret")
+    private String secret;
+
     @Autowired
     void setIlearnUserMapper(IlearnUserMapper ilearnUserMapper) {
         this.ilearnUserMapper = ilearnUserMapper;
+    }
+
+    @Autowired
+    void setRestTemplate(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
     }
 
     @Override
@@ -41,7 +59,7 @@ public class WXAuthorize implements AuthorizeService {
             ILearnException.cast("微信授权登录失败");
         }
         // 拿着授权码去申请令牌
-
+        Map<?, ?> wxAccessToken = getWXAccessToken(code);
         // 用令牌去调用微信的用户信息服务获取用户信息
 
         // 将用户信息保存进数据库
@@ -57,5 +75,31 @@ public class WXAuthorize implements AuthorizeService {
         ilearnUserAuthorities.setAuthorities(userAuthorities);
         // 返回用户的授权信息
         return ilearnUserAuthorities;
+    }
+
+    /**
+     * 获取请求微信服务用的令牌
+     *
+     * @param code 用户授权码
+     * @return 微信令牌
+     */
+    private Map<?, ?> getWXAccessToken(String code) {
+        // url模板
+        String url_template = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=%s&secret=%s&code=%s&grant_type=authorization_code";
+        // 向模板中填充数据
+        String url = String.format(url_template, appID, secret, code);
+        /* 使用restTemplate远程调用微信服务拿到令牌, 返回结果:
+        {
+            "access_token":"ACCESS_TOKEN",
+            "expires_in":7200,
+            "refresh_token":"REFRESH_TOKEN",
+            "openid":"OPENID",
+            "scope":"SCOPE",
+            "unionid": "o6_bmasdasdsad6_2sgVt7hMZOPfL"
+        } */
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.POST, null, String.class);
+        String body = response.getBody();
+        // 将json数据转成Map
+        return JsonUtil.jsonToMap(body);
     }
 }
