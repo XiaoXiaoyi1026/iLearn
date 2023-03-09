926687d77d3d0975a0eb772b3bd1fb298a0b8751
xiaoxiaoyi <xiaoxiaoyi1026@gmail.com>
2023-03-09 11:10:48 +0800

    部署学习中心服务

---
+++ iLearn-generator/src/main/java/com/ilearn/learning_center/mapper/IlearnChooseCourseMapper.java
@@ -0,0 +1,16 @@
+package com.ilearn.learning_center.mapper;
+
+import com.ilearn.learning_center.model.po.IlearnChooseCourse;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.springframework.stereotype.Repository;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author xiaoxiaoyi
+ */
+public interface IlearnChooseCourseMapper extends BaseMapper<IlearnChooseCourse> {
+
+}
---
+++ iLearn-generator/src/main/java/com/ilearn/learning_center/service/IlearnCourseTablesService.java
@@ -0,0 +1,16 @@
+package com.ilearn.learning_center.service;
+
+import com.ilearn.learning_center.model.po.IlearnCourseTables;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author xiaoxiaoyi
+ * @since 2023-03-09
+ */
+public interface IlearnCourseTablesService extends IService<IlearnCourseTables> {
+
+}
